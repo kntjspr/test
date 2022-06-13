3572f84dd6ddc1b83871f952faa2a742b6f07794
000 <65537922+kntjspr@users.noreply.github.com>
2022-06-14 05:23:29 +0800

    Add files via upload

---
+++ ai.py
@@ -0,0 +1,24 @@
+import random, decimal
+import numpy as np
+from decimal import *
+
+def rndFloat(min, max, decimals, size, addchars=''): 
+    #Reference: https://stackoverflow.com/questions/2891790/how-to-pretty-print-a-numpy-array-without-scientific-notation-and-with-given-pre
+    #size = number of iterations, addchars = covert float to string and concatenate
+    decimals = "{: 0." + str(decimals) + "f}" # formatter={'float': {: 0." + str(decimals) + "f}.format}
+    result = np.random.random(size) * (max - min) + min #(math min-max formula neural network)
+    np.set_printoptions(formatter={'float': decimals.format},suppress=True)
+    if not addchars:
+        return (result)
+    else:
+        result = [str(arr) + str(addchars) for arr in result]
+        return result
+
+def rndChoice(chars, size=1):
+    return np.random.choice(chars, size=size, replace=True)
+
+def main():
+
+
+if __name__=="__main__":
+    main()
